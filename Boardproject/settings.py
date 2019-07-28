e3fdb6dc3fe0967b72a116b0a17c32f64fe5fb87
LEE <invisibleWarehouse>
2019-07-29 00:14:21 +0900

    please

--- Boardproject/settings.py
+++ Boardproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
